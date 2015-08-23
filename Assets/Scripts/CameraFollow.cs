98b5e7e9f42709c56b9d000f9802a386f1bafdfd
matheuscarius <matheus.carius37@gmail.com>
2015-08-23 01:04:02 -0300

    Adiciona Colisor Inferior e Lateral e Limites da Camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,8 +3,10 @@
 public class CameraFollow : MonoBehaviour {
     public GameObject player;
     public float delta;
+    public float xMin, xMax;
     void Update() {
         float x = Mathf.Clamp(transform.position.x, player.transform.position.x - delta, player.transform.position.x + delta);
+        x = Mathf.Clamp(x, xMin, xMax);
         transform.position = new Vector3(x, transform.position.y, transform.position.z);
     }
 }
