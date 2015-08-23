caee3bd08b8f6b86a5d85cda645bb5d9b4d6b481
matheuscarius <matheus.carius37@gmail.com>
2015-08-23 11:03:33 -0300

    Adiciona Monster Spawner

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,6 +5,8 @@ public class ButtonManager : MonoBehaviour {
 
     public GameObject bridge;
     public GameObject bridgeColider;
+    public GameObject elfPrefab;
+    bool spawned;
 
     AudioSource audio;
 
@@ -14,6 +16,7 @@ public class ButtonManager : MonoBehaviour {
     {
         GameObject explosion = GameObject.Find("Explosion");
         audio = explosion.GetComponent<AudioSource>();
+        spawned = false;
     }
 
     public void Activate()
@@ -23,6 +26,16 @@ public class ButtonManager : MonoBehaviour {
             audio.Play();
             bridge. SetActive(!bridge.activeSelf);
             bridgeColider.SetActive(!bridgeColider.activeSelf);
+
+            //Monster Spawn
+            if (!spawned)
+            {
+                Instantiate(elfPrefab, transform.position + (Vector3.down) * 3, Quaternion.identity);
+                Instantiate(elfPrefab, transform.position + (Vector3.down) * 3 + (Vector3.left) * 2, Quaternion.identity);
+                Instantiate(elfPrefab, transform.position + (Vector3.down) * 3 + (Vector3.left) * 4, Quaternion.identity);
+                spawned = true;
+            }
+
         }
     }
 }
---
+++ Assets/Scripts/MonsterSpawner1.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterSpawner1 : MonoBehaviour {
+
+    public GameObject elfPrefab;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.tag == "Player")
+        {
+            Instantiate(elfPrefab, transform.position + (Vector3.right) * 5, Quaternion.identity);
+            Instantiate(elfPrefab, transform.position + (Vector3.left) * 5, Quaternion.identity);
+
+            if(coll.transform.position.y > transform.position.y)
+                Instantiate(elfPrefab, transform.position + (Vector3.down) * 2, Quaternion.identity);
+
+            else
+                Instantiate(elfPrefab, transform.position + (Vector3.up) * 2, Quaternion.identity);
+
+            Destroy(this);
+        }
+    }
+}
