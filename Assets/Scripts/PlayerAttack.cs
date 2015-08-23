560b02b471dc7f014b26690639ad9f5419a9cd4f
matheuscarius <matheus.carius37@gmail.com>
2015-08-23 16:07:06 -0300

    Adiciona Controles Gamepad

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -11,7 +11,7 @@ public class PlayerAttack : MonoBehaviour {
     }
     
 	void Update () {
-        if (Input.GetKey(keyAttack) && !weapon.isAttacking) {
+        if (Input.GetButton("Action") && !weapon.isAttacking) {
             weapon.Attack();
             GetComponent<Animator>().SetTrigger("Attack");
         }
