b201a8523c8b00d879cf8a95e954a157a08d332c
Rebeca Calazans <rebecac.brito@gmail.com>
2015-08-24 16:55:37 -0300

    Adicionada princesa

---
+++ Assets/Scripts/Princess.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Princess : MonoBehaviour {
+
+    Vector3 velocity = new Vector3(6f, 0, 0);
+
+	void Update () {
+        transform.position += velocity*Time.deltaTime;
+	}
+    void OnTriggerExit2D(Collider2D col){
+        if (col.gameObject.tag == "MainCamera")
+            Destroy(this.gameObject);
+    }
+}
